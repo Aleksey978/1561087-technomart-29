93b57c12ac32505425ef5c425bac437fb5daec19
Aleksey978 <70477391+Aleksey978@users.noreply.github.com>
2020-10-20 13:57:59 +0300

    слайдер

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 const come = document.querySelector(".user-button");
 const loginPopup = document.querySelector(".modal-login");
 const loginClose = loginPopup.querySelector(".modal-close");
+const perf = document.querySelector(".perf");
+const slidebtn1 = document.querySelector(".catalog-prev");
+const dot1 = document.querySelector(".disabled");
 come.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.add("modal-show");
@@ -8,4 +11,9 @@ come.addEventListener("click", function (evt) {
 loginClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.remove("modal-show");
+});
+slidebtn1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  perf.classList.add("perf-slide");
+  dot1.classlist.add("active-btn");
 });

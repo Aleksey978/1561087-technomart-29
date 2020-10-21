aede64e5d04f75c2f152211ad816f5c14e900c9a
Aleksey978 <70477391+Aleksey978@users.noreply.github.com>
2020-10-21 15:12:11 +0300

    слайдер

--- js/login.js
+++ js/login.js
@@ -4,16 +4,23 @@ const loginClose = loginPopup.querySelector(".modal-close");
 const perf = document.querySelector(".perf");
 const slidebtn1 = document.querySelector(".catalog-prev");
 const dot1 = document.querySelector(".disabled");
-come.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  loginPopup.classList.add("modal-show");
+const slidebtn2 = document.querySelector(".catalog-next");
+
+
+  come.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    loginPopup.classList.add("modal-show");
 });
 loginClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.remove("modal-show");
 });
-slidebtn1.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  perf.classList.add("perf-slide");
-  dot1.classlist.add("active-btn");
+  slidebtn1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    perf.classList.add("perf-slide");
+    dot1.classlist.add("active-btn");
+});
+  slidebtn2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    perf.classList.remove("perf-slide");
 });
---
+++ js/proba.js
@@ -0,0 +1,25 @@
+const servicebtn1 = document.querySelector(".delivery-btn");
+const servicebtn2 = document.querySelector(".Warranty-btn");
+const servicebtn3 = document.querySelector(".credit-btn");
+const slide1 = document.querySelector(".delivery");
+const slide2 = document.querySelector(".Warranty");
+const slide3 = document.querySelector(".credit");
+
+servicebtn2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  slide2.classList.add("slide-show");
+  slide1.classList.remove ("slide-show");
+  slide3.classList.remove("slide-show");
+  });
+servicebtn1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide2.classList.remove("slide-show");
+    slide1.classList.add ("slide-show");
+    slide3.classList.remove("slide-show");
+    });
+servicebtn3.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        slide3.classList.add("slide-show");
+        slide1.classList.remove("slide-show");
+        slide2.classList.remove("slide-show");
+        });

2775923429a3723d4c95903b23b8d120945e6f99
Aleksey978 <70477391+Aleksey978@users.noreply.github.com>
2020-10-26 14:06:44 +0300

    фильтр

--- js/login.js
+++ js/login.js
@@ -3,24 +3,39 @@ const loginPopup = document.querySelector(".modal-login");
 const loginClose = loginPopup.querySelector(".modal-close");
 const perf = document.querySelector(".perf");
 const slidebtn1 = document.querySelector(".catalog-prev");
-const dot1 = document.querySelector(".disabled");
 const slidebtn2 = document.querySelector(".catalog-next");
+const dot1 = document.querySelector(".disabled");
+const dot2 = document.querySelector(".active");
 
-
-  come.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    loginPopup.classList.add("modal-show");
+come.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
 });
 loginClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.remove("modal-show");
 });
-  slidebtn1.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    perf.classList.add("perf-slide");
-    dot1.classlist.add("active-btn");
+slidebtn1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  perf.classList.add("perf-slide");
+  dot1.classList.add("active-btn");
+  dot2.classList.remove("active-btn");
 });
-  slidebtn2.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    perf.classList.remove("perf-slide");
+slidebtn2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  perf.classList.remove("perf-slide");
+  dot2.classList.add("active-btn");
+  dot1.classList.remove("active-btn");
+});
+dot1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  perf.classList.add("perf-slide");
+  dot1.classList.add("active-btn");
+  dot2.classList.remove("active-btn");
+});
+dot2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  perf.classList.remove("perf-slide");
+  dot2.classList.add("active-btn");
+  dot1.classList.remove("active-btn");
 });
--- js/proba.js
+++ js/proba.js
@@ -10,16 +10,25 @@ servicebtn2.addEventListener("click", function (evt) {
   slide2.classList.add("slide-show");
   slide1.classList.remove ("slide-show");
   slide3.classList.remove("slide-show");
+  servicebtn1.classList.remove("nav-section-active");
+  servicebtn2.classList.add("nav-section-active");
+  servicebtn3.classList.remove("nav-section-active");
   });
 servicebtn1.addEventListener("click", function (evt) {
     evt.preventDefault();
     slide2.classList.remove("slide-show");
     slide1.classList.add ("slide-show");
     slide3.classList.remove("slide-show");
-    });
+    servicebtn1.classList.add("nav-section-active");
+    servicebtn2.classList.remove("nav-section-active");
+    servicebtn3.classList.remove("nav-section-active");
+});
 servicebtn3.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        slide3.classList.add("slide-show");
-        slide1.classList.remove("slide-show");
-        slide2.classList.remove("slide-show");
-        });
+    evt.preventDefault();
+    slide3.classList.add("slide-show");
+    slide1.classList.remove("slide-show");
+    slide2.classList.remove("slide-show");
+    servicebtn1.classList.remove("nav-section-active");
+    servicebtn2.classList.remove("nav-section-active");
+    servicebtn3.classList.add("nav-section-active");
+});
